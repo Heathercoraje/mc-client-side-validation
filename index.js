19487d0b1ee879be987ec7a07a4bbc917db693f0
heathercoraje <yhk7830@hotmail.com>
2017-08-14 11:18:54 +0300

    first attempt though i failed brutally

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('hello');
+// document.querySelector('form').addEventListener("submit", function() {
+//   var elements = document.getElementsByTagName("INPUT");
+//   console.log('Katia');
+//   for (var i = 0; i < elements.length; i++) {
+//     elements[i].oninvalid = function(e) {
+//       e.target.setCustomValidity("");
+//       if (!e.target.validity.valid) {
+//         e.target.setCustomValidity("This field cannot be left blank");
+//       }
+//     };
+//     elements[i].oninput = function(e) {
+//       e.target.setCustomValidity("");
+//     };
+//   }
+// });
+
+
+//maybe have 2 add event listeners
+// so one can check before submit ('input')
+// the other can check whether all the values input are correct ('submit')
